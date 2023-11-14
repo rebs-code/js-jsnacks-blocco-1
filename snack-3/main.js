90ed9a5ce79d094f3111c219f860d9b243fef3dc
rebs-code <jack.rebussi@gmail.com>
2023-11-14 09:51:30 +0100

    feat: ciclo for & do-while

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,15 +1,29 @@
 "use strict";
 
-
-
-//Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+//Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
 let myArray = [];
 let sum = 0;
 
-
+//con ciclo while
 while (sum < 50) {
-    let userNumber = parseInt(prompt("Inserisci un numero"));
-    myArray.push(userNumber);
-    sum += userNumber;
-    console.log(sum);
+  let userNumber = parseInt(prompt("Inserisci un numero"));
+  myArray.push(userNumber);
+  sum += userNumber;
+  console.log(sum);
 }
+
+//con ciclo for
+for (let i = 0; sum < 50; i++) {
+  let userNumber = parseInt(prompt("Inserisci un numero"));
+  myArray.push(userNumber);
+  sum += userNumber;
+  console.log(sum);
+}
+
+//con ciclo do while
+do {
+  let userNumber = parseInt(prompt("Inserisci un numero"));
+  myArray.push(userNumber);
+  sum += userNumber;
+  console.log(sum);
+} while (sum < 50);
